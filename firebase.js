594d2e2c83c4ff4a962b5e338058e2ee86b8c378
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-07-29 18:55:17 +0530

    first commit using nextjs12

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import * as firebase from "firebase";
+import { initializeApp } from "firebase/app";
+//import { getFirestore } from "firebase/firestore";
+//import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = !firebase.apps.length ? initializeApp(firebaseConfig) : firebase.apps[0];
+const db = firebase.firestore(app);
+const storage = firebase.storage(app);
+
+export { app, db, storage };
