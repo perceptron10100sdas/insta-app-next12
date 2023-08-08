b4e76efa267292e486169cef9f21d7d1e2d9fa72
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-08-08 20:02:15 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,12 +7,12 @@ import  "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyD5QxKW-jn88IguITNcpVGdiu66fmWOjaA",
+  authDomain: "insta-f9f27.firebaseapp.com",
+  projectId: "insta-f9f27",
+  storageBucket: "insta-f9f27.appspot.com",
+  messagingSenderId: "534350783129",
+  appId: "1:534350783129:web:27d9f1a8dceb7c033c0e08"
 };
 
 // Initialize Firebase
